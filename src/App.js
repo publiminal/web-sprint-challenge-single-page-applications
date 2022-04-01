64efa458d48b0dea547a8851684a25629670b934
Andres Bernal <publiminal@gmail.com>
2022-03-31 23:20:32 -0400

    added css header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+    <div className="shell">
+      <section className="bg-header"  >
+        <header id="home" className="header-top">
+          <h1>Lambda Eats</h1>
+          <nav className="links">
+            <a href="index.html">Home</a>
+            <a href="about.html">Help</a>
+          </nav>  
+        </header>
+      </section>
+
+      <section className="featured">
+        <div className="f-box pic-feat pic-me ">
+          <div className="sh-me">
+            <div className="f-descr">
+            <h2>Your favorite food,<br/> delivered while coding !</h2>
+              <h3 className="bg-projects">
+                <a href="projects.html">My Projects</a>
+              </h3>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="articles">
+        <div className="a-box bg-1">
+          <div className="a-box-sh"><p>I love strings</p></div>
+        </div>
+        <div className="a-box bg-2">
+          <div className="a-box-sh"><p>in instruments</p></div>
+        </div>
+        <div className="a-box bg-3">
+          <div className="a-box-sh"><p>cat hair</p></div>
+        </div>
+
+      </section>      
+
+
+
+
+
+    </div> {/* end shell */}
+
+
+
+
+
+
+      
+     
     </>
   );
 };
